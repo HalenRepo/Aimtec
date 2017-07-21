0de39e835753c6889afde75d7d2310b579f10aa2
HalenRepo <tim3025@msn.com>
2017-07-22 01:37:19 +1000

    Initial commit

---
+++ Heaven Blitz/Program.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Reflection;
+using Aimtec;
+using Aimtec.SDK.Extensions;
+using Aimtec.SDK.Menu.Components;
+using Aimtec.SDK.Orbwalking;
+using Aimtec.SDK.Util;
+
+namespace Heaven_Blitz
+{
+	private static void Main(string[] args)
+	{
+		Console.WriteLine("Hello.");
+	}
+	
+	
+}
